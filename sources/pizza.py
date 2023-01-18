bc9e1f0b40f2fd8f05df495398a611d6a5e64217
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-01-18 10:34:36 +0100

    New Pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'regina': {
+        'ingredients': {'mozzarella': 3, 'ham': 4, 'mushroom': 3},
+        'price': 12,
+    },
 }
